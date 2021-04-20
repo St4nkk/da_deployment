73f772a535d732a42f627bbeb7aa6e6c803133b1
Stanisław Tomasz Krześniak <s.krzesniak@stud.elka.pw.edu.pl>
2021-04-20 18:10:50 +0200

    test1

---
+++ main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 20 15:57:36 2021
+
+@author: Stanisław Krześniak
+"""
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+    
